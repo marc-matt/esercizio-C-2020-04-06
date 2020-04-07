a2dcea207f920b1b08f73ffd8da0c9134f050ef4
marc-matt <62201769+marc-matt@users.noreply.github.com>
2020-04-07 10:27:32 +0200

    Update esercizio-C-2020-04-06.c
    DONE

--- src/esercizio-C-2020-04-06.c
+++ src/esercizio-C-2020-04-06.c
@@ -5,4 +5,30 @@
  *      Author: marco
  */
 
+#include <stdio.h>
+#include <stdbool.h>
 
+void findBruteForce(int * arr, int dimension){
+  bool visited[dimension];
+  for(int i = 0; i < dimension; i++){
+    int x = arr[i];
+    if(visited[i] == false){
+      bool isDuplicated = false;
+      for(int j = i + 1; j < dimension; j++){
+        if(x == arr[j]){
+          isDuplicated = true;
+          visited[j] = true;
+        }
+      }
+      if(!isDuplicated){
+        printf("Element %d appear only once in array\n", x);
+      }
+    }
+  }
+}
+
+int main(int argc, char * argv[]) {
+  int arr[] = {1, 5, 6, 2, 1, 6, 4, 3, 2, 5, 3};
+  findBruteForce(arr, 11);
+  return 0;
+}
